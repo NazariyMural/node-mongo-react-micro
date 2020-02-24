ed534d6aef18f5ec29114a2accb729eac9a8755d
Nazariy Mural <nazik_94@ukr.net>
2020-02-24 11:57:09 +0200

    Fix deprecation warnings

--- videos/server.js
+++ videos/server.js
@@ -1,7 +1,7 @@
 const app = require("./src/app");
 const { DB_URI } = require("./src/config");
 const mongoose = require("mongoose");
-mongoose.connect(DB_URI);
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: });
 
 app.listen(3000, () => {
   console.log("Videos service is running on port 3000");
